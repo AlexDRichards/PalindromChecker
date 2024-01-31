6b0a4c395d48641fe62345e07c7bf14faedd083c
Alex Richards <alexrichards@Alexs-Mac-mini.local>
2024-01-30 19:04:56 -0700

    added functional palindrom logic that passes all FCC tests.

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-console.log(document.getElementById('userInput'))
+// /^[a-zA-Z]+$/
+
+// VARIABLES
+
+const form = document.getElementById("form");
+const textInput = document.getElementById('text-input')
+const log = document.getElementById("log");
+
+// FUNCTIONS
+
+function cleanString(input) {
+  return input.replace(/[ \/\\_.|&;$%@"<>()+,-]/g, "").toLowerCase();
+}
+
+function checkSingleLetter(input) {
+  // console.log(input)
+  return input.length === 1; 
+}
+
+function checkMultiLetter(input) {
+  // console.log(input)
+  if (input.length < 2) {
+    return false;
+  }
+
+  let reversedStringArray = [];
+  stringArray = input.split('');
+  stringArray.forEach(element => {
+    reversedStringArray.unshift(element);
+  });
+  console.log(reversedStringArray.join())
+  return stringArray.join('') === reversedStringArray.join('');
+}
 
 function logSubmit(event) {
-//   log.textContent = `Form Submitted! Timestamp: ${event.timeStamp}`;
-  console.log(document.getElementById('userInput').value)
+  let clnString = cleanString(textInput.value);
+  console.log(clnString);
+  console.log(checkSingleLetter(clnString));
+  console.log(checkMultiLetter(clnString));
+  if (checkSingleLetter(clnString)) {
+    log.innerHTML = `${textInput.value} is a palindrome`;
+  }
+
+  if (checkMultiLetter(clnString)) {
+    log.innerHTML = `${textInput.value} is a palindrome`;
+  } else {
+    log.innerHTML =`${textInput.value} is not a palindrome`;
+  }
+
   event.preventDefault();
 }
 
-const form = document.getElementById("form");
-const log = document.getElementById("log");
+
 form.addEventListener("submit", logSubmit);
